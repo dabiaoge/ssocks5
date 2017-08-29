fbad7788ebd76723107e1869c86c95fb0ba1b6e6
大表哥 <wuqz@live.com>
2017-08-29 10:24:43 +0800

    Update ssocks5.py

--- ssocks5.py
+++ ssocks5.py
@@ -1,68 +1,26 @@
 #!/usr/bin/env python
-
-# Copyright (c) 2013 clowwindy
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-# @2013.08.22 by felix021
-# This file is modified fron local.py from shadowsocks 1.3.3 to act as a pure
-# socks5 proxy.
-# usage:
-#    python ssocks5.py          #listens on 7070
-#    python ssocks5.py 1080     #listens on 1080
-
-from __future__ import with_statement
+# -*- coding:utf-8 -*-
 import sys
-
-try:
-    import gevent
-    import gevent.monkey
-    gevent.monkey.patch_all(dns=gevent.version_info[0] >= 1)
-except ImportError:
-    gevent = None
-    print >>sys.stderr, 'warning: gevent not found, using threading instead'
-
 import socket
 import select
 import SocketServer
 import struct
-import os
 import logging
-import getopt
-
-
-def send_all(sock, data):
-    bytes_sent = 0
-    while True:
-        r = sock.send(data[bytes_sent:])
-        if r < 0:
-            return r
-        bytes_sent += r
-        if bytes_sent == len(data):
-            return bytes_sent
-
 
 class ThreadingTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     allow_reuse_address = True
 
-
 class Socks5Server(SocketServer.StreamRequestHandler):
+    def send_all(self,sock, data):
+        bytes_sent = 0
+        while True:
+            r = sock.send(data[bytes_sent:])
+            if r < 0:
+                return r
+            bytes_sent += r
+            if bytes_sent == len(data):
+                return bytes_sent
+
     def handle_tcp(self, sock, remote):
         try:
             fdset = [sock, remote]
@@ -72,7 +30,7 @@ class Socks5Server(SocketServer.StreamRequestHandler):
                     data = sock.recv(4096)
                     if len(data) <= 0:
                         break
-                    result = send_all(remote, data)
+                    result = self.send_all(remote, data)
                     if result < len(data):
                         raise Exception('failed to send all data')
 
@@ -80,7 +38,7 @@ class Socks5Server(SocketServer.StreamRequestHandler):
                     data = remote.recv(4096)
                     if len(data) <= 0:
                         break
-                    result = send_all(sock, data)
+                    result = self.send_all(sock, data)
                     if result < len(data):
                         raise Exception('failed to send all data')
         finally:
@@ -128,32 +86,17 @@ class Socks5Server(SocketServer.StreamRequestHandler):
         except socket.error, e:
             logging.warn(e)
 
-
 def main():
-    global PORT, LOCAL, IPv6
-    
-    logging.basicConfig(level=logging.DEBUG,
-                        format='%(asctime)s %(levelname)-8s %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S', filemode='a+')
-
-    # fix py2exe
-    if hasattr(sys, "frozen") and sys.frozen in \
-            ("windows_exe", "console_exe"):
-        p = os.path.dirname(os.path.abspath(sys.executable))
-        os.chdir(p)
-
-    KEY = None
-    METHOD = None
-    LOCAL = ''
-    IPv6 = False
-    
     PORT = 7070
+    LOCAL = ''
+
     if len(sys.argv) > 1:
         PORT = int(sys.argv[1])
 
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)-8s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S', filemode='a+')
     try:
-        if IPv6:
-            ThreadingTCPServer.address_family = socket.AF_INET6
         server = ThreadingTCPServer((LOCAL, PORT), Socks5Server)
         logging.info("starting local at %s:%d" % tuple(server.server_address[:2]))
         server.serve_forever()
@@ -162,6 +105,6 @@ def main():
     except KeyboardInterrupt:
         server.shutdown()
         sys.exit(0)
-        
+
 if __name__ == '__main__':
     main()
